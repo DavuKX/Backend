7c782af652b15f8f33b44fc695e448ab93156225
DavuKX <duvandavid2001@gmail.com>
2023-04-03 21:47:18 -0500

    Create routes to handle sessions

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,12 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use http\Cookie;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
     public function user(): string
     {
-        return 'Authenticated user';
+        return Auth::user();
+    }
+
+    public function register(Request $request): User
+    {
+        return User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        if(!Auth::attempt($request->only('email', 'password')))
+        {
+            return response([
+                'message' => 'Invalid credentials'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $user = Auth::user();
+
+        $token = $user->createToken('session')->plainTextToken;
+        $cookie = \cookie('jwt', $token, 60 * 24);
+
+        return response([
+            'message' => 'Login success'
+        ])->withCookie($cookie);
+    }
+
+    public function logout()
+    {
+        $cookie = \Illuminate\Support\Facades\Cookie::forget('jwt');
+
+        return response([
+            'message' => 'Log out successful'
+        ])->withCookie($cookie);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +13,12 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::prefix('user')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
 
-Route::get('user', [AuthController::class, 'user']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/', [AuthController::class, 'user']);
+        Route::post('/logout', [AuthController::class, 'logout']);
+    });
+});
