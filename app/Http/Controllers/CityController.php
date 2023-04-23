166fc253869456151f4eca60a259a0d41465c03d
DavuKX <duvandavid2001@gmail.com>
2023-04-23 14:14:39 -0500

    create cities crud

---
+++ app/Http/Controllers/CityController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCityRequest;
+use App\Http\Requests\UpdateCityRequest;
+use App\Models\City;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Foundation\Application;
+use Symfony\Component\HttpFoundation\Response;
+
+class CityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): Application|\Illuminate\Http\Response|\Illuminate\Contracts\Foundation\Application|ResponseFactory
+    {
+        return response(City::all(), Response::HTTP_OK);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCityRequest $request): Application|\Illuminate\Http\Response|\Illuminate\Contracts\Foundation\Application|ResponseFactory
+    {
+        $data = $request->validated();
+
+        $city = City::create($data);
+
+        return response($city, Response::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(City $city): Application|\Illuminate\Http\Response|\Illuminate\Contracts\Foundation\Application|ResponseFactory
+    {
+        return response($city, Response::HTTP_OK);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCityRequest $request, City $city): Application|\Illuminate\Http\Response|\Illuminate\Contracts\Foundation\Application|ResponseFactory
+    {
+        $data = $request->validated();
+
+        if ($city->update($data))
+        {
+            return response($city, Response::HTTP_OK);
+        }
+
+        return response(null, Response::HTTP_INTERNAL_SERVER_ERROR);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(City $city): Application|\Illuminate\Http\Response|\Illuminate\Contracts\Foundation\Application|ResponseFactory
+    {
+        if ($city->delete())
+        {
+            return response(null, Response::HTTP_NO_CONTENT);
+        }
+
+        return response(null, Response::HTTP_INTERNAL_SERVER_ERROR);
+    }
+}
